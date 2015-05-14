64c10b32ec678d2962351867696d2f72f82a691c
XingGaoY <yxg13@mails.tsinghua.edu.cn>
2015-05-14 11:50:43 +0800

    perlin noise
    generate white noise for perlin noise

---
+++ white_noise.cpp
@@ -0,0 +1,24 @@
+#include <opencv2/highgui/highgui.hpp>
+#include <iostream>
+#include <string>
+#include <random>
+using namespace std;
+int main()
+{
+	random_device rd;
+	mt19937 mt(rd());
+	uniform_int_distribution<> dis(0,255);
+	using namespace cv;
+	Mat workspace(500,700,CV_8UC3,Scalar(0,0,0));
+	for(int i=0;i<500;i++)
+	{
+		for(int j=0;j<700;j++)
+		{
+			int c = mt();
+			workspace.at<Vec3b>(i,j) = Vec3b(c,c,c);
+		}
+
+	}
+	imshow("white noise",workspace);
+	waitKey();
+}
